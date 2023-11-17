aa93f295f9a9fddcac7c4bdf576485c09f912130
Antonio-Zuniga-Lovera <122412774+Antonio-Zuniga-Lovera@users.noreply.github.com>
2023-11-17 03:41:08 -0300

    Calculando total v1.0

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+function formatearNumeroConPuntos(number) {
+    const parts = number.toString().split('.');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+    return parts.join('.');
+};
+
+const amount = document.querySelector('#Amount');
+const buttonAdd = document.querySelector('#Button-add');
+const buttonEliminate = document.querySelector('#Button-eliminate');
+let totalQuantity = document.querySelector('#Total-quantity');
+
+let counter = 0;
+let price = 719990;
+let totalPrice;
+
+
+buttonAdd.addEventListener('click', () => {
+    if(counter >= 0) {
+        counter++;
+        totalPrice = price * counter;
+        amount.innerHTML = `Cantidad: ${counter}`;
+        totalQuantity.innerHTML = `Total a pagar: $${(formatearNumeroConPuntos(totalPrice))}`;
+    }
+    console.log(counter);
+    console.log(totalPrice);
+});
+
+buttonEliminate.addEventListener('click', () => {
+    if(counter > 0) {
+        counter--;
+        var totalPrice = price * counter;
+        amount.innerHTML = `Cantidad: ${counter}`;
+        totalQuantity.innerHTML = `Total a pagar: $${formatearNumeroConPuntos(totalPrice)}`;
+    } else {
+        alert('Debes agregar al menos 1 producto');
+    }
+    /* Imprimo en la consola la variable contador para ver si obtiene valores negativos*/
+    console.log(counter);
+    console.log(totalPrice);
+});
